e1a310b522cc3efc014e4d4dbab9a41713a4c16f
Yi Miao <yilin.miao@alumni.stonybrook.edu>
2015-11-22 23:16:03 -0500

    final draft

--- app/src/main/java/com/yilinmiao/myappportfolio/MainActivity.java
+++ app/src/main/java/com/yilinmiao/myappportfolio/MainActivity.java
@@ -1,5 +1,6 @@
 package com.yilinmiao.myappportfolio;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +21,81 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        final Button button = (Button) findViewById(R.id.button);
+        final Button button2 = (Button) findViewById(R.id.button2);
+        final Button button3 = (Button) findViewById(R.id.button3);
+        final Button button4 = (Button) findViewById(R.id.button4);
+        final Button button5 = (Button) findViewById(R.id.button5);
+        final Button button6 = (Button) findViewById(R.id.button6);
+        button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Context context = getApplicationContext();
+                CharSequence text = "This button will launch my Spotify Streamer App!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                CharSequence text = "This button will launch my Football Scores App!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                CharSequence text = "This button will launch my Library App!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                CharSequence text = "This button will launch my Build It Bigger App!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                CharSequence text = "This button will launch my XYZ Reader App!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                CharSequence text = "This button will launch my Capstone App!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
     }
 
     @Override
